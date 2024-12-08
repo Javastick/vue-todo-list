42d1b389e2f0cc4c6e9a7d390ade2ba5b94f68cc
Ziamul Umam <ziamulumamsssss@gmail.com>
2024-12-08 16:53:37 +0700

    menyelesaikan dibuku dan mengedit tampilan

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue';
+import TodoList from './components/TodoList.vue';
+</script>
+
+<template>
+  <div class="content-wrap">
+    <AppHeader />
+    <TodoList />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.content-wrap{
+  display: flex;
+  flex-direction: column;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: block;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
